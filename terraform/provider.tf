779c090258d0ffe1600ea9d8c029773b3d835330
Daniel Velásquez Iturrate <velasqueziturrate@gmail.com>
2026-08-15 20:21:23 +0200

    Adding terraform provider

---
+++ terraform/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      Project     = "Swedbank"
+      Owner       = "daniel.velasquez"
+      Environment = "dev"
+      ManagedBy   = "terraform"
+    }
+  }
+}
